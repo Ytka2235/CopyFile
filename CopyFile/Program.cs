146d99a8563b94ca31e36dc0a3781123466c33ca
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-21 08:54:24 +0300

    Добавьте файлы проекта.

---
+++ CopyFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Последовательное копирование");
+        Timer timer = new Timer();
+        timer.Start();
+        string src1 = @"C:\Users\admin_0\Desktop\file1.txt";
+        string dest1 = @"C:\Users\admin_0\Desktop\cory1.txt";
+        File.Copy(src1, dest1);
+        string src2 = @"C:\Users\admin_0\Desktop\file2.txt";
+        string dest2 = @"C:\Users\admin_0\Desktop\cory2.txt";
+        File.Copy(src2, dest2);
+        timer.Stop();
+        Console.WriteLine("Паралельное копирование");
+        Thread th = new Thread(Copy);
+        timer.Start();
+        th.Start();
+        string src = @"C:\Users\admin_0\Desktop\file1.txt";
+        string dest = @"C:\Users\admin_0\Desktop\cory3.txt";
+        File.Copy(src, dest);
+        timer.Stop();
+
+    }
+
+
+    public static void Copy()
+    {
+        string src = @"C:\Users\admin_0\Desktop\file2.txt";
+        string dest = @"C:\Users\admin_0\Desktop\cory4.txt";
+        File.Copy(src, dest);
+    }
+}
---
+++ CopyFile/Timer.cs
@@ -0,0 +1,19 @@
+﻿internal class Timer
+{
+    public DateTime dt1;
+    public DateTime dt2;
+    public Timer() /// Конструктор создаёт таймер вызывать так: Timer timer = new Timer();
+    {
+
+    }
+    public void Start() /// запускает таймер вызывать так: timer.Start();
+    {
+        dt1 = DateTime.Now;
+    }
+    public void Stop() /// останавливает таймер и выводит сколько времени прошло с начала запуска вызывать так: Timer.Stop();
+    {
+        dt2 = DateTime.Now;
+        TimeSpan ts = dt2 - dt1;
+        Console.WriteLine("Времени прошло " + ts.TotalMilliseconds + " миллисекунда"); /// время отоброжается в миллисекундах
+    }
+}

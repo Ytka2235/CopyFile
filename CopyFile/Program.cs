e1725372ddd047e308bec457d7d62ada64fd21b0
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-21 09:01:16 +0300

    gotovo

--- CopyFile/Program.cs
+++ CopyFile/Program.cs
@@ -5,16 +5,23 @@ public class Program
 {
     public static void Main()
     {
+        string src1 = @"C:\Users\admin_0\Desktop\file1.txt";
+        Console.WriteLine("Введите название файла");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите путь файла");
+        string way = Console.ReadLine();
+        File.Copy(src1, (@"" + way + name));
+
         Console.WriteLine("Последовательное копирование");
         Timer timer = new Timer();
         timer.Start();
-        string src1 = @"C:\Users\admin_0\Desktop\file1.txt";
         string dest1 = @"C:\Users\admin_0\Desktop\cory1.txt";
         File.Copy(src1, dest1);
         string src2 = @"C:\Users\admin_0\Desktop\file2.txt";
         string dest2 = @"C:\Users\admin_0\Desktop\cory2.txt";
         File.Copy(src2, dest2);
         timer.Stop();
+
         Console.WriteLine("Паралельное копирование");
         Thread th = new Thread(Copy);
         timer.Start();
